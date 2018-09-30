bf7aa3bd621d97ebd3cd94bfac20dfcbc95838ef
aquinaswuwua <tomasz.wojciechowski84@gmail.com>
2018-09-30 09:39:53 +0200

    navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import SignedInLink from './SignedInLink'
+import SignedOutLink from './SignedOutLink'
 
 const Navbar = () => {
 
@@ -7,6 +9,8 @@ const Navbar = () => {
         <nav className="nav-wrapper grey darken">
             <div className="container">
                 <Link to='/' className="brand-logo">MarioPlan</Link>
+                <SignedInLink/>
+                <SignedOutLink/>
             </div>
         </nav>
     )
