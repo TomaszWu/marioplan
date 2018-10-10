c0d4c126389fbdc2b04f1015c8be54ea67ce019c
aquinaswuwua <tomasz.wojciechowski84@gmail.com>
2018-10-10 20:47:36 +0200

    19 done

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -9,10 +9,14 @@ const initState = {
 const projectReducer = (state = initState, action) => {
   switch (action.type) {
     case 'CREATE_PROJECT':
-      console.log('creatted project', action.project)
+      console.log('creatted project', action.project);
+      return state;
+    case 'CREATE_PROJECT_ERROR':
+      console.log('create project error', action.err);
+      return state;
+    default:
+      return state;
   }
-  
-  return state;
 }
 
 export default projectReducer
