f15d4d4ad802996d47b6220885571edd4434225c
aquinaswuwua <tomasz.wojciechowski84@gmail.com>
2018-10-17 21:24:29 +0200

    30 and 31 done

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -3,6 +3,7 @@ import {connect} from 'react-redux'
 import {firestoreConnect} from 'react-redux-firebase'
 import {compose} from 'redux'
 import { Redirect } from 'react-router-dom'
+import moment from 'moment'
 
 const ProjectDetails = (props) => {
 
@@ -21,7 +22,7 @@ if (project) {
         </div>
         <div className="card-action grey lighten-4 grey-text">
           <div>Posted by {project.authorFirstName} {project.authorLastName}</div>
-          <div>2nd September, 2am</div>
+          <p className="grey-text">{moment(project.createdAt.toDate()).calendar()}</p>
         </div>
       </div>
     </div>
--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -3,11 +3,15 @@ export const createProject = (project) => {
 
     const firestore = getFirestore();
 
+    const profile = getState().firebase.profile;
+
+    const authorId = getState().firebase.auth.uid;
+
     firestore.collection('projects').add({
       ...project,
-      authorFirstName: 'Net',
-      authorLastName: 'Ninja',
-      authorId: 12345,
+      authorFirstName: profile.firstName,
+      authorLastName: profile.lastName,
+      authorId: authorId,
       createdAt: new Date(),
     }).then(() => {
       dispatch({type: 'CREATE_PROJECT', project});
