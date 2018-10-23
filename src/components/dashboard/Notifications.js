9eef50179203687fe880a4715b2f85dc53f692ea
aquinaswuwua <tomasz.wojciechowski84@gmail.com>
2018-10-23 21:15:39 +0200

    35 done

--- src/components/dashboard/Notifications.js
+++ src/components/dashboard/Notifications.js
@@ -3,11 +3,18 @@ import React from 'react'
 const Notifications = () => {
 
     return (
-        <div>
-            <p>Notifications</p>
+        <div className="section">
+            <div className="card z-depth-0">
+              <span className="card-title">Notifications</span>
+              <ul className="notifications">
+                <li>notifications</li>
+                <li>notifications</li>
+                <li>notifications</li>
+                <li>notifications</li>
+              </ul>
+            </div>
         </div>
     )
-
 }
 
 export default Notifications
